50e5cb0c06ec7677d3684b9404ac77909aec6b54
Maxim <purplehorrorrus@gmail.com>
2020-08-17 20:57:15 +0800

    change verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,7 @@ module.exports = {
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: "A personal website" },
-            { name: "google-site-verification", content: "uXrLgCztsUea29CDiaDKGaMj1nPMckQA3g0bcx_0bGM" }
+            { hid: "description", name: "description", content: "A personal website" }
         ],
         link: [
             { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
