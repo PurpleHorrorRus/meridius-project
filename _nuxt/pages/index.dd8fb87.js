47b07181d8b31cab938c8ee9b0be0578b6db6691
Maxim <purplehorrorrus@gmail.com>
2020-09-08 18:43:36 +0800

    57b6140

---
+++ _nuxt/pages/index.dd8fb87.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{167:function(t,n,e){"use strict";e.r(n);var r={},o=e(29),component=Object(o.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("nuxt-link",{attrs:{to:"/meridius"},domProps:{textContent:this._s("Meridius - музыкальный плеер для ВК")}})}),[],!1,null,null,null);n.default=component.exports}}]);
